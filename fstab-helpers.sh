17b4e4859b01810a469beb644fae95276d01042c
5kind <weirui5671@outlook.com>
2023-11-15 19:29:53 +0800

    bug fix

--- fstab-helpers.sh
+++ fstab-helpers.sh
@@ -4,21 +4,25 @@ try_mkdir() {
     warning "$file not exist, try mkdir it firstly!"
     mkdir -p "$file"
   fi
+  shift
+  [[ -z "$@" ]] || try_mkdir $@
 }
 
-try_install() {
+try_create() {
   local file=$1
   if [[ ! -e $file && ! -L $file ]]; then
     warning "$file not exist, try create it firstly!"
     install -Dm644 /dev/null "$file"
   fi
+  shift
+  [[ -z "$@" ]] || try_create $@
 }
 
 try_create_dest() {
   if [[ -b $src || -d $src ]] ;then
     try_mkdir $dest
   elif [[ -e $src ]] ;then
-    try_install $dest
+    try_create $dest
   else
     try_mkdir $dest
   fi
